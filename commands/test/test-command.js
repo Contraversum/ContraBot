d1ffde915d6f76a0becfd9566f3f9ff4347376fe
Simon H <97528607+q-Sci@users.noreply.github.com>
2023-08-07 14:44:24 +0200

    bot DMs users with test stuff

--- commands/test/test-command.js
+++ commands/test/test-command.js
@@ -1,10 +1,23 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const question = (interaction, str) =>
+  interaction.user.send(str).then((sentMessage) => {
+    sentMessage.react('👍');
+    sentMessage.react('🤩');
+    sentMessage.react('👎');
+    sentMessage.react('🤢');
+    sentMessage.react('➖');
+  });
+
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('test')
-		.setDescription('Asks the test questions!'),
-	async execute(interaction) {
-		await interaction.reply('💩');
-	},
+  data: new SlashCommandBuilder().setName('test').setDescription('Asks the test questions!'),
+  async execute(interaction) {
+    await interaction.reply(
+      `Test gestartet.\nBitte beantworte die Fragen des Bots in den Direktnachrichten.`
+    );
+    await interaction.user.send(
+      'Bitte beantworte alle Fragen.\n👍: Ich stimme zu\n🤩: Ich stimme zu (doppelt gewichtet)\n👎: Ich stimme nicht zu\n🤢: Ich stimme nicht zu (doppelt gewichtet)\n➖: Ist mir egal!\nTestfragen:'
+    );
+    await question(interaction, 'Hi!');
+  },
 };
