205497aa4cd8f606d2941fbc15aba9e3fa0fb968
Simon H <97528607+q-Sci@users.noreply.github.com>
2023-08-07 16:42:18 +0200

    small changes

--- commands/test/test-command.js
+++ commands/test/test-command.js
@@ -2,7 +2,9 @@ const { SlashCommandBuilder } = require('discord.js');
 
 const question = (interaction, str) =>
   interaction.user.send(str).then((sentMessage) => {
+    // pre-set reactions by the bot
     sentMessage.react('👍');
+    sentMessage.react('🤩');
     sentMessage.react('🤩');
     sentMessage.react('👎');
     sentMessage.react('🤢');
@@ -12,9 +14,10 @@ const question = (interaction, str) =>
 module.exports = {
   data: new SlashCommandBuilder().setName('test').setDescription('Asks the test questions!'),
   async execute(interaction) {
-    await interaction.reply(
-      `Test gestartet.\nBitte beantworte die Fragen des Bots in den Direktnachrichten.`
-    );
+    await interaction.reply({
+      content: 'Test gestartet.\nBitte beantworte die Fragen des Bots in den Direktnachrichten.',
+      ephemeral: true,
+    });
     await interaction.user.send(
       'Bitte beantworte alle Fragen.\n👍: Ich stimme zu\n🤩: Ich stimme zu (doppelt gewichtet)\n👎: Ich stimme nicht zu\n🤢: Ich stimme nicht zu (doppelt gewichtet)\n➖: Ist mir egal!\nTestfragen:'
     );
